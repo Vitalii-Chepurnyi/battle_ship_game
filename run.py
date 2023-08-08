346c2da93c3ac849d19cb69197f5598a9bfc20a6
Vitalii-Chepurnyi Barry <chepurnyibarry@gmail.com>
2023-08-08 15:26:17 +0000

    Add main game loop and velidation functions for input data

--- run.py
+++ run.py
@@ -63,10 +63,70 @@ def populate_board(board):
         y = random_point(board.size)
         if (x, y) not in board.ships:
             board.add_ship(x, y)
+
+def valid_row_input(row_input):
+    return row_input.upper() in [chr(65 + i) for i in range(8)]
+
+def valid_column_input(col_input, size):
+    return col_input.isdigit() and 0 <= int(col_input) < size
             
-#def play_game():'
+def play_game(computer_board, player_board):
+    """
+    Main game loop where players take turns to make guesses until one of the players
+    Wins the game or 20 turn are completed.
+    """
+    max_turns = 20
+    turn = 1
+    while turn <= max_turns:
+        print("\n" + "=" * 15 + f" Turn {turn}" + "=" * 15)
+        print("Computer's Board:")
+        computer_board.print()
+        print("\nYour Board:")
+        player_board.print()
+
+        if turn % 2 == 1:
+            print("\nComputer's Turn.")
+            x, y = random_point(player_board.size), random_point(player_board.size)
+            result = player_board.guess(x,y)
+            if result == "Hit":
+                scores["computer"] += 1
+                print("Computer hit one of your ships!")
+            else:
+                print("Computer missed your ships.")
+        else:
+            print(f"\nYour Turn (Turns left: {max_turns - turn + 1})")
+            while True:
+                x_letter = input("Enter the row letter (A to H):")
+                y_number = input("Enter the column number (0 to 7):")
+
+                if not valid_row_input(x_letter) or not valid_column_input(y_number, player_board.size):
+                    print("Incorrect input. Please try again!")
+                else:
+                    break
+            
+            x = letter_to_row_index(x_letter)
+            y = number_to_column_index(y_number)
+
+            result = computer_board.guess(x,y)
+
+            if result == "Hit":
+                scores["player"] += 1
+                print("Congratulations! You hit the computer's ship!")
+            else:
+                print("You missed the compute's ships.")
+
+        if scores["computer"] == computer_board.num_ships:
+            print("\nComputer wins the game!")
+            break
+        elif scores["player"] == player_board.num_ships:
+            print("\nCongratulations! You Win The Game!")
+            break   
 
 def new_game():
+    """
+    Starts a new game. Sets the board size and number of ships, resets the
+    Scores and initializes the boards.
+    """
     size = 8
     num_ships = 8
     print("=" * 35)
